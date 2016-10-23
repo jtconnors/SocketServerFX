0705d530fa64e4073e88e8a41fdcb1716de77e64
Jim Connors <github@jtconnors.com>
2016-10-22 23:39:58 -0400

    cosmetic fix to "see also" section of file

---
+++ README.md
@@ -0,0 +1,16 @@
+# SocketServerFX
+
+SocketServerFX is a simple JavaFX 2.x based UI application (generated with
+FXML) representing the server end of a socket connection.  Used in conjunction
+with the SocketClientFX application, these two show how data is passed
+between sockets.
+
+**** NOTE: This application has a dependency on the com.jtconnors.socket
+project which can be downloaded from via git from:
+https://github.com/jtconnors/com.jtconnors.socket.git
+
+See also:
+
+- SocketClientFX: https://github.com/jtconnors/SocketClientFX.git
+
+- MultiSocketServerFX: https://github.com/jtconnors/MultiSocketServerFX.git
---
+++ src/socketserverfx/FXMLDocumentController.java
@@ -0,0 +1,256 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package socketserverfx;
+
+import com.jtconnors.socket.DebugFlags;
+import com.jtconnors.socket.SocketListener;
+import java.lang.invoke.MethodHandles;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import socketfx.FxSocketServer;
+
+
+/**
+ * FXML Controller class
+ *
+ * @author jtconnor
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private ListView<String> rcvdMsgsListView;
+    @FXML
+    private ListView<String> sentMsgsListView;
+    @FXML
+    private Button sendButton;
+    @FXML
+    private TextField sendTextField;
+    @FXML
+    private TextField selectedTextField;
+    @FXML
+    private Button connectButton;
+    @FXML
+    private Button disconnectButton;
+    @FXML
+    private TextField portTextField;
+    @FXML
+    private CheckBox autoConnectCheckBox;
+    @FXML
+    private Label connectedLabel;
+    
+    private final static Logger LOGGER =
+            Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
+
+    private ObservableList<String> rcvdMsgsData;
+    private ObservableList<String> sentMsgsData;
+    private ListView lastSelectedListView;
+
+    private boolean isConnected;
+
+    public enum ConnectionDisplayState {
+
+        DISCONNECTED, WAITING, CONNECTED, AUTOCONNECTED, AUTOWAITING
+    }
+
+    private FxSocketServer socket;
+
+    private void connect() {
+        socket = new FxSocketServer(new FxSocketListener(),
+                Integer.valueOf(portTextField.getText()),
+                DebugFlags.instance().DEBUG_NONE);
+        socket.connect();
+    }
+
+    private void displayState(ConnectionDisplayState state) {
+        switch (state) {
+            case DISCONNECTED:
+                connectButton.setDisable(false);
+                disconnectButton.setDisable(true);
+                sendButton.setDisable(true);
+                sendTextField.setDisable(true);
+                connectedLabel.setText("Not connected");
+                break;
+            case WAITING:
+            case AUTOWAITING:
+                connectButton.setDisable(true);
+                disconnectButton.setDisable(true);
+                sendButton.setDisable(true);
+                sendTextField.setDisable(true);
+                connectedLabel.setText("Waiting to connect");
+                break;
+            case CONNECTED:
+                connectButton.setDisable(true);
+                disconnectButton.setDisable(false);
+                sendButton.setDisable(false);
+                sendTextField.setDisable(false);
+                connectedLabel.setText("Connected");
+                break;
+            case AUTOCONNECTED:
+                connectButton.setDisable(true);
+                disconnectButton.setDisable(true);
+                sendButton.setDisable(false);
+                sendTextField.setDisable(false);
+                connectedLabel.setText("Connected");
+                break;
+        }
+    }
+
+    /**
+     * Initializes the controller class.
+     *
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        isConnected = false;
+        displayState(ConnectionDisplayState.DISCONNECTED);
+
+        sentMsgsData = FXCollections.observableArrayList();
+        sentMsgsListView.setItems(sentMsgsData);
+        sentMsgsListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        sentMsgsListView.setOnMouseClicked((Event event) -> {
+            String selectedItem
+                    = sentMsgsListView.getSelectionModel().getSelectedItem();
+            if (selectedItem != null && !selectedItem.equals("null")) {
+                selectedTextField.setText("Sent: " + selectedItem);
+                lastSelectedListView = sentMsgsListView;
+            }
+        });
+
+        rcvdMsgsData = FXCollections.observableArrayList();
+        rcvdMsgsListView.setItems(rcvdMsgsData);
+        rcvdMsgsListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        rcvdMsgsListView.setOnMouseClicked((Event event) -> {
+            String selectedItem
+                    = rcvdMsgsListView.getSelectionModel().getSelectedItem();
+            if (selectedItem != null && !selectedItem.equals("null")) {
+                selectedTextField.setText("Received: " + selectedItem);
+                lastSelectedListView = rcvdMsgsListView;
+            }
+        });
+
+        Runtime.getRuntime().addShutdownHook(new ShutDownThread());
+
+        /*
+         * Uncomment to have autoConnect enabled at startup
+         */
+//        autoConnectCheckBox.setSelected(true);
+//        displayState(ConnectionDisplayState.WAITING);
+//        connect();
+    }
+
+    class ShutDownThread extends Thread {
+
+        @Override
+        public void run() {
+            if (socket != null) {
+                if (socket.debugFlagIsSet(DebugFlags.instance().DEBUG_STATUS)) {
+                    LOGGER.info("ShutdownHook: Shutting down Server Socket");    
+                }
+                socket.shutdown();
+            }
+        }
+    }
+
+    class FxSocketListener implements SocketListener {
+
+        @Override
+        public void onMessage(String line) {
+            if (line != null && !line.equals("")) {
+                rcvdMsgsData.add(line);
+            }
+        }
+
+        @Override
+        public void onClosedStatus(boolean isClosed) {
+            if (isClosed) {
+                isConnected = false;
+                if (autoConnectCheckBox.isSelected()) {
+                    displayState(ConnectionDisplayState.AUTOWAITING);
+                    connect();
+                } else {
+                    displayState(ConnectionDisplayState.DISCONNECTED);
+                }
+            } else {
+                isConnected = true;
+                if (autoConnectCheckBox.isSelected()) {
+                    displayState(ConnectionDisplayState.AUTOCONNECTED);
+                } else {
+                    displayState(ConnectionDisplayState.CONNECTED);
+                }
+            }
+        }
+    }
+
+    @FXML
+    private void handleClearRcvdMsgsButton(ActionEvent event) {
+        rcvdMsgsData.clear();
+        if (lastSelectedListView == rcvdMsgsListView) {
+            selectedTextField.clear();
+        }
+    }
+
+    @FXML
+    private void handleClearSentMsgsButton(ActionEvent event) {
+        sentMsgsData.clear();
+        if (lastSelectedListView == sentMsgsListView) {
+            selectedTextField.clear();
+        }
+    }
+
+    @FXML
+    private void handleSendMessageButton(ActionEvent event) {
+        if (!sendTextField.getText().equals("")) {
+            socket.sendMessage(sendTextField.getText());
+            sentMsgsData.add(sendTextField.getText());
+        }
+    }
+
+    @FXML
+    private void handleConnectButton(ActionEvent event) {
+        displayState(ConnectionDisplayState.WAITING);
+        connect();
+    }
+
+    @FXML
+    private void handleDisconnectButton(ActionEvent event) {
+        displayState(ConnectionDisplayState.DISCONNECTED);
+        socket.shutdown();
+    }
+
+    @FXML
+    private void handleAutoConnectCheckBox(ActionEvent event) {
+        if (autoConnectCheckBox.isSelected()) {
+            if (isConnected) {
+                displayState(ConnectionDisplayState.AUTOCONNECTED);
+            } else {
+                displayState(ConnectionDisplayState.AUTOWAITING);
+                connect();
+            }
+        } else {
+            if (isConnected) {
+                displayState(ConnectionDisplayState.CONNECTED);
+            } else {
+                displayState(ConnectionDisplayState.WAITING);
+            }
+        }
+    }
+
+}
